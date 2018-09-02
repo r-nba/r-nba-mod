258cb31d4688f932eb5bb4b8368d67cadb378687
Tanay Gottigundala <tgottigundala@gmail.com>
2018-09-02 10:27:43 -0700

    Rename praw.py to nbamod.py

--- nba/praw.py
+++
@@ -1,5 +0,0 @@
-import praw
-import config
-
-def create_sidebar():
-    reddit = praw.Reddit(client_id=config.client_id, client_secret=config.client_secret, user_agent=config.user_agent, username=config.username, password=config.password)
