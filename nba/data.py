3766b3221c0ffd6b3cc3ad4dc3f1f37261b32cdb
tanayg <tgottigundala@gmail.com>
2018-09-06 01:06:47 -0700

    Team subreddits and Schedule complete

--- nba/data.py
+++ nba/data.py
@@ -1,38 +1,60 @@
-import urllib
+from urllib.request import urlopen
 import requests
 import json
 import datetime
+import calendar
+
+def main():
+    d = data()
+    d.schedule()
 
 class data:
     def team_subreddits(self):
-        return ""
+        return [{"team_abbrev":abbrev, "subreddit":self.team_abbrev_dict[abbrev]["sub"]} for abbrev in self.team_abbrev_dict]
 
     def schedule(self):
-        return ""
+        days = {}
+        for delta in range(3):
+            date = (datetime.datetime.today()+datetime.timedelta(days=delta))
+            month = calendar.month_name[date.month]
+            calendar_day = calendar.day_name[date.weekday()]
+            key = calendar_day + ", " + month + " " + date.strftime('%d')
+            parameter = date.strftime('%Y%m%d')
+            parameter = str(20180928 + delta) # This line is for testing purposes. Date is set to first day of preseason
+            #print(key)
+            #print(parameter)
+            games = []
+            with urlopen('http://data.nba.com/prod/v2/' + parameter + '/scoreboard.json') as url:
+                j = json.loads(url.read().decode())
+                for game in j["games"]:
+                    if game["vTeam"]["teamId"] not in self.team_id_dict or game["hTeam"]["teamId"] not in self.team_id_dict: # Games against non-nba teams are disregarded
+                        continue
+                    gameDetails = {}
+                    gameDetails["time"] = game["startTimeEastern"].replace(" ET", "")
+                    gameDetails["home"] = game["hTeam"]["triCode"]
+                    gameDetails["home_subreddit"] = self.team_id_dict[game["hTeam"]["teamId"]]["sub"]
+                    gameDetails["away"] = game["vTeam"]["triCode"]
+                    gameDetails["away_subreddit"] = self.team_id_dict[game["vTeam"]["teamId"]]["sub"]
+                    #print(gameDetails)
+                    games.append(gameDetails)
+            #print(games)
+            days[key] = games
+
+        #print(days)
+
+        return days
 
     def game_threads(self):
         return ""
 
     def top_bar(self): # Check Bre's work on r/nbadev if you don't know what this is
-        def reddit_card():
-            return ""
-        
-        def flair_tool_card():
-            return ""
-        
-        def live_game_updates_cards():
-            return ""
         
         top_bar_cards = []
-        top_bar_cards.append(reddit_card())
-        top_bar_cards.append(flair_tool())
-        top_bar_cards.append(live_game_updates())
         return top_bar_cards
 
     def standings(self):
-        """
         standings = {}
-        with urllib.request.urlopen('http://data.nba.com/prod/v1/current/standings_conference.json') as url:
+        with urlopen('http://data.nba.com/prod/v1/current/standings_conference.json') as url:
             j = json.loads(url.read().decode())
 
             for i in range(0,15):
@@ -41,27 +63,24 @@ class data:
                 west = j['league']['standard']['conference']['west'][i]
                 west_id = west['teamId']
                 tmp_row = {
-                    'east_name': self.team_dict[east_id]['short_name'],
-                    'east_nick': self.team_dict[east_id]['med_name'],
+                    'east_name': self.team_id_dict[east_id]['short_name'],
+                    'east_nick': self.team_id_dict[east_id]['med_name'],
                     'east_record': east['win'] + '-' + east['loss'],
                     'east_gb_conf': '%.1f' % int(east['gamesBehind']),
                     'east_div_rank': east['divRank'],
                     'conf_rank': str(i+1),
-                    'west_name': self.team_dict[west_id]['short_name'],
-                    'west_nick': self.team_dict[west_id]['med_name'],
+                    'west_name': self.team_id_dict[west_id]['short_name'],
+                    'west_nick': self.team_id_dict[west_id]['med_name'],
                     'west_record': west['win'] + '-' + west['loss'],
                     'west_gb_conf': '%.1f' % int(west['gamesBehind']),
                     'west_div_rank': west['divRank']
                 }
                 standings[int(i+1)] = tmp_row
         return standings
-        """
-        return ""
 
     def playoffs(self):
-        """
         bracket = {}
-        with urllib.request.urlopen('https://data.nba.com/data/10s/prod/v1/2017/playoffsBracket.json') as url:
+        with urlopen('https://data.nba.com/data/10s/prod/v1/2017/playoffsBracket.json') as url:
             j = json.loads(url.read().decode())
 
             for i in range(0,15):
@@ -72,16 +91,16 @@ class data:
                 top_seed = series['topRow']['teamId']
                 bottom_seed = series['bottomRow']['teamId']
                 
-                if series_winner == self.team_dict[top_seed]['short_name']:
+                if series_winner == self.team_id_dict[top_seed]['short_name']:
                     series_record = series['summaryStatusText'][-3:].replace("-", " - ")
                 else:
                     series_record = series['summaryStatusText'][-3:].replace("-", " - ")[::-1]
 
                 tmp_brkt = {
                     'series': series['confName'] + " R" + series['roundNum'],
-                    'top_name': self.team_dict[top_seed]['short_name'],
+                    'top_name': self.team_id_dict[top_seed]['short_name'],
                     'top_seed': series['topRow']['seedNum'],
-                    'bottom_name': self.team_dict[bottom_seed]['short_name'],
+                    'bottom_name': self.team_id_dict[bottom_seed]['short_name'],
                     'bottom_seed': series['bottomRow']['seedNum'],
                     'summary': series_record
                 }
@@ -98,8 +117,6 @@ class data:
                             'champ': "NA"
                         }
         return bracket
-        """
-        return ""
 
     def __init__(self):        
         self.json_teams = None
@@ -107,13 +124,13 @@ class data:
 
     # Helper functions
     def load_teams(self):
-        self.team_dict = {}
-        self.team_dict_med_key = {}
-        self.team_dict_short_key = {}
+        self.team_id_dict = {}
+        self.team_name_dict = {}
+        self.team_abbrev_dict = {}
         with open('data/teams.csv', 'r') as teamInfoFile:
             for teamInfoRow in teamInfoFile.read().split('\n'):
                 teamInfo = teamInfoRow.split(',')
-                tmp_team_dict = {
+                tmp_team_id_dict = {
                     'long_name': teamInfo[0] + ' ' + teamInfo[1],
                     'med_name': teamInfo[1],
                     'short_name': teamInfo[2].upper(),
@@ -123,6 +140,9 @@ class data:
                     'conference': teamInfo[6],
                     'id': teamInfo[7]
                 }
-                self.team_dict[teamInfo[7]] = tmp_team_dict
-                self.team_dict_med_key[teamInfo[1]] = tmp_team_dict
-                self.team_dict_short_key[teamInfo[2].upper()] = tmp_team_dict
+                self.team_id_dict[teamInfo[7]] = tmp_team_id_dict
+                self.team_name_dict[teamInfo[1]] = tmp_team_id_dict
+                self.team_abbrev_dict[teamInfo[2].upper()] = tmp_team_id_dict
+
+if __name__ == "__main__":
+    main()
