090d9687b4d56995d5aa38181e76eb33c23134c8
sanjee <sanjee.yogeswaran@gmail.com>
2018-09-12 20:09:42 -0500

    added game thread id code

--- nba/data.py
+++ nba/data.py
@@ -3,6 +3,9 @@ import json
 import datetime
 import calendar
 import collections
+import praw
+import re
+from prawmod import bot
 
 def main():
     d = data()
@@ -10,6 +13,19 @@ def main():
     d.playoffs()
 
 class data:
+    teamAbbrs = [
+        'ATL', 'CHA', 'MIA', 'ORL', 'WAS',
+        'BOS', 'BKN', 'NYK', 'PHI', 'TOR',
+        'CHI', 'CLE', 'DET', 'IND', 'MIL',
+        'GSW', 'LAC', 'LAL', 'PHX', 'SAC',
+        'DAL', 'HOU', 'MEM', 'NOP', 'SAS',
+        'DEN', 'MIN', 'OKC', 'POR', 'UTA'
+    ]
+    nicknames = ['Hawks', 'Hornets', 'Heat', 'Magic', 'Wizards', 'Celtics', 'Nets', 'Knicks', '76ers', 'Raptors',
+                 'Bulls', 'Cavaliers', 'Pistons', 'Pacers', 'Bucks', 'Warriors', 'Clippers', 'Lakers', 'Suns', 'Kings',
+                 'Mavericks','Rockets','Grizzlies','Pelicans','Spurs','Nuggets','Timberwolves','Thunder','Trail Blazers',
+                 'Jazz']
+    teams = zip(teamAbbrs,nicknames)
     def team_subreddits(self):
         return [{"team_abbrev":abbrev, "subreddit":self.team_abbrev_dict[abbrev]["sub"]} for abbrev in self.team_abbrev_dict]
 
@@ -45,18 +61,34 @@ class data:
 
         return days
 
+    def getThread(self, hTeamName, vTeamName):
+        for thread in self.bot.subreddit('nba' ).new(limit=200):
+            threadType = str(thread.link_flair_css_class)
+            threadDate =  datetime.datetime.now().replace(hour = 0, minute = 0, second = 0, microsecond = 0)
+
+            if thread.threadType == threadType and thread.threadDate == threadDate and all(x in thread.threadTitle for x in [hTeamName, vTeamName]):
+                return thread.threadID
+
+
     def top_bar(self): # Check Bre's work on r/nbadev if you don't know what this is
         games = []
         parameter = datetime.datetime.today().strftime('%Y%m%d')
         #parameter = str(20180314) # This line is for testing purposes. Date is set to March 14th, 2018.
         parameter = str(20180928) # This line is for testing purposes. Date is set to March 14th, 2018.
-        
+
         with urlopen('http://data.nba.com/prod/v2/' + parameter + '/scoreboard.json') as url:
             j = json.loads(url.read().decode())
             for game in j["games"]:
                 if game["vTeam"]["teamId"] not in self.team_id_dict or game["hTeam"]["teamId"] not in self.team_id_dict: # Games against non-nba teams are disregarded
                     continue
+                for team in self.teams:
+                    if team[0] == game['hTeam']['teamId']:
+                        hTeamName = team[1]
+                    if team[0] == game['vTeam']['teamId']:
+                        vTeamName = team[1]
+                gameThreadID = self.getThread(hTeamName,vTeamName)
                 gameDetails = {}
+                gameDetails['gamethread'] = gameThreadID
                 if game["statusNum"] == 1: # Game hasn't started
                     gameDetails["time"] = game["startTimeEastern"].replace(" ET", "")
                 elif game["statusNum"] == 2: # Game in progress
@@ -155,6 +187,7 @@ class data:
     def __init__(self):        
         self.json_teams = None
         self.load_teams()
+        self.bot = bot
 
     # Helper functions
     def load_teams(self):
--- nba/test.py
+++ nba/test.py
@@ -1,7 +1,21 @@
 from data import data
 from markdown import markdown
 from pprint import pprint
-D = data()
-M = markdown()
-pprint(D.playoffs())
-print(M.playoffs(D.playoffs()))
+from prawmod import bot
+import datetime
+import requests
+import json
+def getThread(hTeamName, vTeamName):
+    for thread in bot.subreddit('nba' ).new(limit=200):
+        threadType = str(thread.link_flair_css_class)
+        threadDate =  datetime.datetime.now().replace(hour = 0, minute = 0, second = 0, microsecond = 0)
+
+        if thread.threadType == threadType and thread.threadDate == threadDate and all(x in thread.threadTitle for x in [hTeamName, vTeamName]):
+            return thread.threadID
+data = requests.get('https://data.nba.com/data/10s/prod/v1/2017/teams.json').json()
+nicknames = []
+
+for teams in data['league']['standard']:
+    nicknames.append(teams['nickname'])
+
+print(nicknames)
