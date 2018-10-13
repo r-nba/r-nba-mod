80e341b66e6bb1857f662fabeff968fc3848bcd3
brexbre <bullsxbre@gmail.com>
2018-10-13 09:27:35 -0500

    removed defunct functions

--- nba/markdown.py
+++ nba/markdown.py
@@ -9,26 +9,12 @@ class markdown:
             # Unpacks team['subreddit'] into {subreddit}
             team_subs += """* []({subreddit})\n""".format(**team)
         return team_subs
-
-    # Input dictionary containing upcoming schedule data
-    # {"Tuesday": [{gameinfo},{gameinfo}], "Wednesday": [{gameinfo},{gameinfo}], "Thursday": [{gameinfo},{gameinfo}]]
-    # Output: Markdown for schedule
-    def schedule(self, dict_schedule):
-        line = '###[](//)\n######[](//)\n\n'
-        for k, day in dict_schedule.items():
-            line += """[{0}| ET](/date)\n""".format(k)
-            line += """|||\n:--|:-:\n"""
-            for game in day:
-                # Unpacks all keys in game into format tags: eg. game['time'] unpacked to {time}
-                line += """[{time}](/tgt)|[{home}](/r/{home_subreddit}) at [{away}](/r/{away_subreddit})\n""" \
-                    .format(**game)
-        return line
-
+    
     # Input dictionary containing live game scores
     # Output: Markdown for game score bar
-    def top_bar(self, top_bar_elements):
+    def game_bar(self, game_bar_elements):
         text = """> \n* [Reddit](//www.reddit.com)\n* [Player Flair Tool](//www.nbaflairbot.herokuapp.com/)\n* [](#A) [Free Agency Tracker](https://www.reddit.com/r/nba/6j2ts)\n"""
-        for game in top_bar_elements:
+        for game in game_bar_elements:
             home_score = ""
             away_score = ""
             if game['home_score']:
@@ -43,15 +29,29 @@ class markdown:
                 .format(game['time'],game['gamethread'], game['home'], game['away'], home_score, away_score)
         text += '*  \n'
         return text
-
+    
     def standings(self, dict_standings):
-        text = """###[](//)\n##[](//)\n\n|WEST|W - L|GB||GB|W - L|EAST\n:-:|:-:|:-:|:-:|:-:|:-:|:-:\n"""
+        text = """###[](//)\n\n##[](//)\n\n|WEST|W - L|GB||GB|W - L|EAST\n:-:|:-:|:-:|:-:|:-:|:-:|:-:\n"""
         for rank,value in dict_standings.items():
             text += """[{west_name}](/r/{west_sub})|{west_record}|{west_gb_conf}|{conf_rank}|{east_gb_conf}|{east_record}|[{east_name}](/r/{east_sub})\n""".format(**value)
         return text
     
+    # Input dictionary containing upcoming schedule data
+    # {"Tuesday": [{gameinfo},{gameinfo}], "Wednesday": [{gameinfo},{gameinfo}], "Thursday": [{gameinfo},{gameinfo}]]
+    # Output: Markdown for schedule
+    def schedule(self, dict_schedule):
+        line = '###[](//)\n######[](//)\n\n'
+        for k, day in dict_schedule.items():
+            line += """[{0}| ET](/date)\n""".format(k)
+            line += """|||\n:--|:-:\n"""
+            for game in day:
+                # Unpacks all keys in game into format tags: eg. game['time'] unpacked to {time}
+                line += """[{time}](/tgt)|[{home}](/r/{home_subreddit}) at [{away}](/r/{away_subreddit})\n""" \
+                    .format(**game)
+        return line
+
     def playoffs(self, dict_playoffs):
-        text = """###[](//)\n####[](//)\n\n||||||||\n:-:|:-:|:-:|:-:|:-:|:-:|:-:\n**1***8*||**4***5*||**3***6*||**2***7*\n"""
+        text = """###[](//)\n\n####[](//)\n\n||||||||\n:-:|:-:|:-:|:-:|:-:|:-:|:-:\n**1***8*||**4***5*||**3***6*||**2***7*\n"""
 
         # West Round 1
         for i in range(1,5):
@@ -104,104 +104,3 @@ class markdown:
 
         text += """**1***8*||**4***5*||**3***6*||**2***7*\n"""
         return text
-
-
-    # Helper functions
-    def get_legacy_standings(self, dict_standings):
-        legacy_standings = 'WEST|||EAST|||\n:---:|:---:|:---:|:---:|:---:|:---:\n**TEAM**|*W/L*|*GB*|**TEAM**|*W/L*|*GB*\n'
-
-        for i in range(0, 15):
-            standings = dict_standings[i + 1]
-            if i < 8:
-                legacy_standings = legacy_standings + standings['conf_rank'] + ' [](/' + standings['west_name'] + ')|' + \
-                                   standings['west_record'] + '|' + standings['west_gb_conf'] + '|' + standings[
-                                       'conf_rank'] + ' [](/' + standings['east_name'] + ')|' + standings[
-                                       'east_record'] + '|' + standings['east_gb_conf'] + '\n'
-            else:
-                legacy_standings = legacy_standings + '[](/' + standings['west_name'] + ')|' + standings[
-                    'west_record'] + '|' + standings['west_gb_conf'] + '|[](/' + standings['east_name'] + ')|' + \
-                                   standings['east_record'] + '|' + standings['east_gb_conf'] + '\n'
-
-        return legacy_standings
-
-    def get_widget_standings(self, dict_standings):
-        widget_standings = '- \n - \n     - west\n     - W - L\n     - GB\n     - GB\n     - W - L\n     - east\n\n'
-
-        for i in range(0, 15):
-            standings = dict_standings[i + 1]
-            widget_standings = widget_standings + '- \n - \n     - [](/' + standings['west_name'] + ')\n     - ' + \
-                               standings['west_gb_conf'] + '\n     - ' + standings['west_record'] + '\n - ' + standings[
-                                   'conf_rank'] + '\n - [](/#)\n     - [](/' + standings['east_name'] + ')\n     - ' + \
-                               standings['east_gb_conf'] + '\n     - ' + standings['east_record'] + '\n\n'
-
-        return widget_standings
-
-    def get_widget_bracket(self, dict_bracket):
-        bracket = dict_bracket
-
-        widget_bracket = (
-                    "- \n - \n     - 1\n     - 8\n - \n     - 4\n     - 5\n - \n     - 3\n     - 6\n - \n     - 2\n     - 7\n\n" +
-                    "- \n - \n     - [](/" + bracket['1']['top_name'] + ")\n     - [](/" + bracket['1'][
-                        'bottom_name'] + ")\n" +
-                    " - \n     - [](/" + bracket['2']['top_name'] + ")\n     - [](/" + bracket['2'][
-                        'bottom_name'] + ")\n - \n" +
-                    "     - [](/" + bracket['3']['top_name'] + ")\n     - [](/" + bracket['3'][
-                        'bottom_name'] + ")\n - \n  " +
-                    "   - [](/" + bracket['4']['top_name'] + ")\n     - [](/" + bracket['4']['bottom_name'] + ")\n\n" +
-                    "- \n - \n     - " + bracket['1']['top_name'] + "\n     - " + bracket['1'][
-                        'summary'] + "\n     - " + bracket['1']['bottom_name'] +
-                    "\n - \n     - " + bracket['2']['top_name'] + "\n     - " + bracket['2']['summary'] + "\n     - " +
-                    bracket['2']['bottom_name'] +
-                    "\n - \n     - " + bracket['3']['top_name'] + "\n     - " + bracket['3']['summary'] + "\n     - " +
-                    bracket['3']['bottom_name'] +
-                    "\n - \n     - " + bracket['4']['top_name'] + "\n     - " + bracket['4']['summary'] + "\n     - " +
-                    bracket['4']['bottom_name'] + "\n\n" +
-                    "- \n - \n     - [](/" + bracket['9']['top_name'] + ")\n     - [](/" + bracket['9'][
-                        'bottom_name'] + ")\n" +
-                    " - \n     - [](/" + bracket['10']['top_name'] + ")\n     - [](/" + bracket['10'][
-                        'bottom_name'] + ")\n\n" +
-                    "- \n - \n     - " + bracket['9']['top_name'] + "\n     - " + bracket['9'][
-                        'summary'] + "\n     - " + bracket['9']['bottom_name'] +
-                    "\n - \n     - " + bracket['10']['top_name'] + "\n     - " + bracket['10'][
-                        'summary'] + "\n     - " + bracket['10']['bottom_name'] + "\n\n" +
-                    "- \n - \n     - [](/" + bracket['13']['top_name'] + ")\n - \n     - " + bracket['13'][
-                        'top_name'] + "\n     - " + bracket['13']['summary'] +
-                    "\n     - " + bracket['13']['bottom_name'] + "\n - \n     - [](/" + bracket['13'][
-                        'bottom_name'] + ")\n\n" +
-                    "- \n - \n     - [](/" + bracket['15']['top_name'] + ")\n\n" +
-                    "- \n - \n     - [](/" + bracket['16']['champ'] + ")\n - \n     - " + bracket['15'][
-                        'top_name'] + "\n     - " + bracket['15']['summary'] +
-                    "\n     - " + bracket['15']['bottom_name'] + "\n\n- \n - \n     - [](/" + bracket['15'][
-                        'bottom_name'] + ")\n\n" +
-                    "- \n - \n     - [](/" + bracket['14']['top_name'] + ")\n - \n     - " + bracket['14']['top_name'] +
-                    "\n     - " + bracket['14']['summary'] + "\n     - " + bracket['14'][
-                        'bottom_name'] + "\n - \n     - [](/" + bracket['14']['bottom_name'] + ")\n\n" +
-                    "- \n - \n     - " + bracket['11']['top_name'] + "\n     - " + bracket['11'][
-                        'summary'] + "\n     - " + bracket['11']['bottom_name'] +
-                    "\n - \n     - " + bracket['12']['top_name'] + "\n     - " + bracket['12'][
-                        'summary'] + "\n     - " + bracket['12']['bottom_name'] + "\n\n" +
-                    "- \n - \n     - [](/" + bracket['11']['top_name'] + ")\n     - [](/" + bracket['11'][
-                        'bottom_name'] + ")\n" +
-                    " - \n     - [](/" + bracket['12']['top_name'] + ")\n     - [](/" + bracket['12'][
-                        'bottom_name'] + ")\n\n" +
-                    "- \n - \n     - " + bracket['5']['top_name'] + "\n     - " + bracket['5'][
-                        'summary'] + "\n     - " + bracket['5']['bottom_name'] +
-                    "\n - \n     - " + bracket['6']['top_name'] + "\n     - " + bracket['6']['summary'] + "\n     - " +
-                    bracket['6']['bottom_name'] +
-                    "\n - \n     - " + bracket['7']['top_name'] + "\n     - " + bracket['7']['summary'] + "\n     - " +
-                    bracket['7']['bottom_name'] +
-                    "\n - \n     - " + bracket['8']['top_name'] + "\n     - " + bracket['8']['summary'] + "\n     - " +
-                    bracket['8']['bottom_name'] + "\n\n" +
-                    "- \n - \n     - [](/" + bracket['5']['top_name'] + ")\n     - [](/" + bracket['5'][
-                        'bottom_name'] + ")\n" +
-                    " - \n     - [](/" + bracket['6']['top_name'] + ")\n     - [](/" + bracket['6'][
-                        'bottom_name'] + ")\n - \n" +
-                    "     - [](/" + bracket['7']['top_name'] + ")\n     - [](/" + bracket['7'][
-                        'bottom_name'] + ")\n - \n  " +
-                    "   - [](/" + bracket['8']['top_name'] + ")\n     - [](/" + bracket['8']['bottom_name'] + ")\n\n" +
-                    "- \n - \n     - 1\n     - 8\n - \n     - 4\n     - 5\n - \n     - 3\n     - 6\n - \n     - 2\n     - 7")
-
-        return widget_bracket
-
-
-
